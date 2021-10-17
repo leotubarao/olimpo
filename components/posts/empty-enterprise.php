580055e935411d96cbf94671e358af7216961e73
Leônidas Santos <oi@ltco.com.br>
2021-10-17 15:01:40 -0300

    fix: alter enterprise error message

--- components/posts/empty-enterprise.php
+++ components/posts/empty-enterprise.php
@@ -1,5 +1,5 @@
 <li class="ltco_enterprises__list__item error">
   <p class="h6 text-danger">
-    Não encontramos nada em <?= the_title_enterprise($args['content']); ?>
+    Não encontramos nada!
   </p>
 </li>
